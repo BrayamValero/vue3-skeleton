24980fce6cde7316de0ad6008863432c25c7569b
Brayam Valero <bvalerop@gmail.com>
2024-03-17 20:29:28 -0400

    fix: renamed package name

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "vue3-skeleton",
+      name: "Vue3Skeleton",
       fileName: "vue3-skeleton",
     },
     rollupOptions: {
