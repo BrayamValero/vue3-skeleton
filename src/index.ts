21754e80ca4c1305c8840075f3e22f0e889b107a
Brayam Valero <bvalerop@gmail.com>
2024-03-17 21:27:57 -0400

    fix: renaming package imports & require files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Import all the components here, there are more elegant ways to do this but this is the simplest
 import VueSkeleton from "./components/Skeleton.vue";
 
-export { VueSkeleton };
+export default VueSkeleton;
